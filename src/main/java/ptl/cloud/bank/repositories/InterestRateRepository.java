1750ba4bf8f66442f4fbb9f8bfe90d6466df67f5
ptylczynski <piotr@ptl.cloud>
2022-05-28 14:50:40 +0200

    add: interest rate repository

---
+++ src/main/java/ptl/cloud/bank/repositories/InterestRateRepository.java
@@ -0,0 +1,6 @@
+package ptl.cloud.bank.repositories;
+
+import ptl.cloud.bank.entities.InterestRate;
+
+public interface InterestRateRepository extends OwnerSearchableRepository<InterestRate, Long> {
+}
