15e9f00a0e31c9d94b7ede50905eac8cdc2a4ba7
ptylczynski <piotr@ptl.cloud>
2022-05-28 14:45:59 +0200

    init

---
+++ src/test/java/ptl/cloud/bank/BankApplicationTests.java
@@ -0,0 +1,12 @@
+package ptl.cloud.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
